42d3da092625542352ac00840ef58df9bbd9705d
longxuanho <longxuanho@gmail.com>
2017-06-07 21:07:34 +0700

    Update thanh-toan

--- src/app/san-phams/gio-hang/gio-hang.component.ts
+++ src/app/san-phams/gio-hang/gio-hang.component.ts
@@ -9,19 +9,27 @@ import { CartService, ItemCartModel } from '../../core/shared/cart.service';
 export class GioHangComponent implements OnInit {
 
   cartItems: ItemCartModel[];
+  totalPrice: number = 0;
 
   constructor(private cartService: CartService) { }
 
   removeItemFromCart(item: ItemCartModel) {
-    console.log(this.cartItems.indexOf(item), item);
     this.cartItems.splice(this.cartItems.indexOf(item), 1);
-    console.log(this.cartItems);
+    this.onResolveCart();    
+  }
+
+  onResolveCart() {
+    this.totalPrice = 0;
+    this.cartItems.forEach(item => {
+      item.thanhTien = item.soLuong * item.donGia;
+      this.totalPrice += item.thanhTien;
+    });
     this.cartService.saveCart(this.cartItems);
   }
 
   ngOnInit() {
     this.cartItems = this.cartService.getCart();
-    console.log(this.cartItems);
+    this.onResolveCart();
   }
 
 }
--- src/app/san-phams/thanh-toan/thanh-toan.component.ts
+++ src/app/san-phams/thanh-toan/thanh-toan.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService, ItemCartModel } from '../../core/shared/cart.service';
+
 @Component({
   selector: 'sk-thanh-toan',
   templateUrl: './thanh-toan.component.html',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThanhToanComponent implements OnInit {
 
-  constructor() { }
+  cartItems: ItemCartModel[];
+  totalPrice: number = 0;
+
+  constructor(private cartService: CartService) { }
+
+  onResolveCart() {
+    this.totalPrice = 0;
+    this.cartItems.forEach(item => {
+      item.thanhTien = item.soLuong * item.donGia;
+      this.totalPrice += item.thanhTien;
+    });
+    this.cartService.saveCart(this.cartItems);
+  }
 
   ngOnInit() {
+    this.cartItems = this.cartService.getCart();
+    this.onResolveCart();
   }
 
 }
